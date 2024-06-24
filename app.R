b8bd37e62a1fe29f3199dc5ce1aa8e5f85966b46
Mike McHenry <mm8658a@american.edu>
2024-06-23 20:53:40 -0400

    Add moneypuck data and load libraries

--- app.R
+++ app.R
@@ -1,13 +1,17 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+## Shiny app for NHL dashboard
 
+
+# Load Libraries ----------------------------------------------------------
 library(shiny)
+library(tidyverse)
+library(data.table)
+
+# Load in Data ------------------------------------------------------------
+
+team <- read_csv("./Data/MoneyPuck/team_data.csv")
+
+
+# Define User Interface ---------------------------------------------------
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
