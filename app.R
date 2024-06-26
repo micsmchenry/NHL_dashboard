f5a9a2a18dca5a5c304518dc17a07f613c478b62
Mike McHenry <mm8658a@american.edu>
2024-06-26 15:12:12 -0400

    Created new scripts folder for data cleaning

--- app.R
+++ app.R
@@ -8,7 +8,56 @@ library(data.table)
 
 # Load in Data ------------------------------------------------------------
 
-team <- read_csv("./Data/MoneyPuck/team_data.csv")
+mp_df <- read_csv("./Data/moneypuck_team_data.csv") |>
+  filter(season == 2023)
+
+# Loads 2023 data
+download.file("https://moneypuck.com/moneypuck/playerData/seasonSummary/2023/regular/teams.csv", "./Data/test.csv")
+
+
+# Create Team Names -------------------------------------------------------
+
+# NOTE: Need to address teams that have relocated
+team_names <- mp_df |>
+  rename("Team" = "team") |>
+  mutate(Team = case_when(
+    Team == "VGK" ~ "Vegas Golden Knights",
+    Team == "NYR" ~ "New York Rangers",
+    Team == "BOS" ~ "Boston Bruins",
+    Team == "COL" ~ "Colorado Avalanche",
+    Team == "VAN" ~ "Vancouver Canucks",
+    Team == "LAK" ~ "Los Angeles Kings",
+    Team == "FLA" ~ "Florida Panthers",
+    Team == "DET" ~ "Detroit Red Wings",
+    Team == "DAL" ~ "Dallas Stars",
+    Team == "WPG" ~ "Winnipeg Jets",
+    Team == "CAR" ~ "Carolina Hurricanes",
+    Team == "TBL" ~ "Tampa Bay Lightning",
+    Team == "PHI" ~ "Philadelphia Flyers",
+    Team == "ARI" ~ "Arizona Coyotes",
+    Team == "TOR" ~ "Toronto Maple Leafs",
+    Team == "STL" ~ "St. Louis Blues",
+    Team == "NYI" ~ "New York Islanders",
+    Team == "NSH" ~ "Nashville Predators",
+    Team == "WSH" ~ "Washington Capitals",
+    Team == "NJD" ~ "New Jersey Devils",
+    Team == "PIT" ~ "Pittsburgh Penguins",
+    Team == "MTL" ~ "Montreal Canadiens",
+    Team == "CGY" ~ "Calgary Flames",
+    Team == "BUF" ~ "Buffalo Sabres",
+    Team == "MIN" ~ "Minnesota Wild",
+    Team == "SEA" ~ "Seattle Kraken",
+    Team == "EDM" ~ "Edmonton Oilers",
+    Team == "CBJ" ~ "Columbus Blue Jackets",
+    Team == "OTT" ~ "Ottawa Senators",
+    Team == "ANA" ~ "Anaheim Ducks",
+    Team == "SJS" ~ "San Jose Sharks",
+    Team == "CHI" ~ "Chicago Blackhawks"
+  )) |>
+  distinct(Team) |>
+  arrange(Team)
+
+
 
 
 # Define User Interface ---------------------------------------------------
@@ -17,7 +66,11 @@ team <- read_csv("./Data/MoneyPuck/team_data.csv")
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("NHL Dashboard"),
+    
+    selectInput("team",
+                "Select Team",
+                team_names),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
